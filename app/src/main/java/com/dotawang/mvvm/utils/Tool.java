32738e854ac6148404539f2edd08e2ece551e36b
gitwangyang <gitwangyang@aliyun.com>
2018-10-24 14:10:38 +0800

    工具类tool

--- app/src/main/java/com/dotawang/mvvm/utils/Tool.java
+++ app/src/main/java/com/dotawang/mvvm/utils/Tool.java
@@ -1,16 +1,33 @@
 package com.dotawang.mvvm.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.dotawang.mvvm.app.AppApplication;
 
+import java.io.File;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -669,6 +686,8 @@ public class Tool {
         return false;
     }
 
+    //-------------------------------------Toast吐丝------------------------------------
+
     private static Handler handler = new Handler(Looper.getMainLooper());
     public static void showToast(final String message) {
 
@@ -695,6 +714,8 @@ public class Tool {
         });
 
     }
+
+    //-------------------------------------EditText输入内容限制------------------------------------
 
     /**
      * 禁止EditText输入空格和换行符
@@ -753,5 +774,414 @@ public class Tool {
             editText.setText(str1);
             editText.setSelection(str1.length());
         }
+    }
+
+    //-------------------------------------版本号比较------------------------------------
+
+    /**
+     * 比较app的版本号
+     * @param oldVersion
+     * @param newVersion
+     * @return
+     */
+    public static int compareVersion(String oldVersion, String newVersion) {
+        if (null == newVersion || "".equals(newVersion) || null == oldVersion || "".equals(oldVersion)) {
+            return 0;
+        }
+        String[] v1 = oldVersion.split("\\.");
+        String[] v2 = newVersion.split("\\.");
+        if (v1.length < v2.length) { // 如果版本.长度不一样，后面的缺省为0，如果保证长度一样，这里可以不需要
+            String[] t = new String[v2.length];
+            System.arraycopy(v1, 0, t, 0, v1.length);
+            for (int i = v1.length; i < t.length; i++) {
+                t[i] = "0";
+            }
+            v1 = t;
+        } else if (v1.length > v2.length) {
+            String[] t = new String[v1.length];
+            System.arraycopy(v2, 0, t, 0, v2.length);
+            for (int i = v2.length; i < t.length; i++) {
+                t[i] = "0";
+            }
+            v2 = t;
+        }
+
+        for (int i = 0; i < v1.length; i++) {
+            int n1 = Integer.valueOf(v1[i]).intValue();
+            int n2 = Integer.valueOf(v2[i]).intValue();
+            if (n1 != n2) {
+                return (n1 - n2); // 大于0则s1版本大，小于0则s2版本大
+            }
+        }
+        return 0; // 相等
+    }
+
+    public static boolean getUninatllApkInfo(Context context, String filePath) {
+        boolean result = false;
+        try {
+            PackageManager pm = context.getPackageManager();
+            Log.e("archiveFilePath", filePath);
+            PackageInfo info = pm.getPackageArchiveInfo(filePath,
+                    PackageManager.GET_ACTIVITIES);
+//			    String packageName = null;
+            if (info != null) {
+                result = true;
+            }
+        } catch (Exception e) {
+            result = false;
+            Log.e("install error", "*****  解析未安装的 apk 出现异常 *****" + e.getMessage());
+        }
+        return result;
+    }
+
+    public static void openFile(File file, Context context) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //设置intent的Action属性
+        intent.setAction(Intent.ACTION_VIEW);
+        //获取文件file的MIME类型
+        String type = Tool.getMIMEType(file);
+        //设置intent的data和Type属性。
+        intent.setDataAndType(/*uri*/Uri.fromFile(file), type);
+        //跳转
+        context.startActivity(intent);
+    }
+
+    /**
+     * 根据文件后缀名获得对应的MIME类型。
+     *
+     * @param file
+     */
+    public static String getMIMEType(File file) {
+        String type = "*/*";
+        String fName = file.getName();
+        //获取后缀名前的分隔符"."在fName中的位置。
+        int dotIndex = fName.lastIndexOf(".");
+        if (dotIndex < 0) {
+            return type;
+        }
+        /* 获取文件的后缀名 */
+        String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+        if (end == "") {
+            return type;
+        }
+        //在MIME和文件类型的匹配表中找到对应的MIME类型。
+        for (int i = 0; i < MIME_MapTable.length; i++) {
+            if (end.equals(MIME_MapTable[i][0])) {
+                type = MIME_MapTable[i][1];
+            }
+        }
+        return type;
+    }
+
+    //建立一个MIME类型与文件后缀名的匹配表
+    private static final String[][] MIME_MapTable = {
+            //{后缀名，    MIME类型}
+            {".3gp", "video/3gpp"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "*/*"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-javascript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/x-mpeg"},
+            {".mp4", "video/mp4"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/png"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".prop", "text/plain"},
+            {".rar", "*/*"},
+            {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"},
+            {".rtf", "application/rtf"},
+            {".sh", "text/plain"},
+            {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"},
+            {".txt", "text/plain"},
+            {".wav", "audio/x-wav"},
+            {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/x-ms-wmv"},
+            {".wps", "application/vnd.ms-works"},
+            {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/zip"},
+            {"", "*/*"}
+    };
+
+    //是否包含中文
+    public static boolean isContainChinese(String str) {
+        Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+        Matcher m = p.matcher(str);
+        if (m.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    //-------------------------------------手机屏幕宽高计算------------------------------------
+
+    private static int sScreenWidth = -1;
+    private static int sScreenHeight = -1;
+
+    /**
+     * 获取屏幕宽度
+     *
+     * @return 屏幕宽度
+     */
+    public static int getScreenWidth() {
+        if (sScreenWidth == -1) {
+            sScreenWidth = AppApplication.getInstance().getResources().getDisplayMetrics().widthPixels;
+        }
+        return sScreenWidth;
+    }
+
+    /**
+     * 获取屏幕高度
+     *
+     * @return 屏幕高度
+     */
+    public static int getScreenHeight() {
+        if (sScreenHeight == -1) {
+            sScreenHeight = AppApplication.getInstance().getResources().getDisplayMetrics().heightPixels;
+        }
+        return sScreenHeight;
+    }
+
+    //-------------------------------------字符串转换------------------------------------
+    /**
+     * 字符串转float
+     *
+     * @param floatValue   float型字符串
+     * @param defaultValue 默认值
+     * @return float数值
+     */
+    public static float parseFloat(String floatValue, float defaultValue) {
+        float value;
+        try {
+            value = Float.parseFloat(floatValue);
+        } catch (Exception e) {
+            value = defaultValue;
+        }
+        return value;
+    }
+
+    /**
+     * 字符串转long
+     *
+     * @param longValue    字符型long
+     * @param defaultValue 默认值
+     * @return long型数值
+     */
+    public static long parseLong(String longValue, long defaultValue) {
+        long value;
+        try {
+            value = Long.parseLong(longValue);
+        } catch (Exception e) {
+            value = defaultValue;
+        }
+        return value;
+    }
+
+    /**
+     * 字符串转int
+     *
+     * @param intValue     字符型int
+     * @param defaultValue 默认值
+     * @return long型数值
+     */
+    public static int parseInt(String intValue, int defaultValue) {
+        int value;
+        try {
+            value = Integer.parseInt(intValue);
+        } catch (Exception e) {
+            value = defaultValue;
+        }
+        return value;
+    }
+
+    /**
+     * @param doubleValue  字符串double
+     * @param defaultValue 默认值
+     * @return double数值
+     */
+    public static double parseDouble(String doubleValue, double defaultValue) {
+        double value;
+        try {
+            value = Double.parseDouble(doubleValue);
+        } catch (Exception e) {
+            value = defaultValue;
+        }
+        return value;
+    }
+
+
+    //-------------------------------------虚拟按键------------------------------------
+    /**
+     * 获取是否存在NavigationBar，是否有虚拟按钮
+     */
+    public static boolean isNavigationBarShow(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Display display = activity.getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            Point realSize = new Point();
+            display.getSize(size);
+            display.getRealSize(realSize);
+            return realSize.y != size.y;
+        } else {
+            boolean menu = ViewConfiguration.get(activity).hasPermanentMenuKey();
+            boolean back = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+            if (menu || back) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    /**
+     * 获取虚拟按钮ActionBar的高度
+     *
+     * @param activity activity
+     * @return ActionBar高度
+     */
+    public static int getNavigationBarHeight(Activity activity) {
+        if (!isNavigationBarShow(activity)) {
+            return 0;
+        }
+        Resources resources = activity.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height",
+                "dimen", "android");
+        //获取NavigationBar的高度
+        int height = resources.getDimensionPixelSize(resourceId);
+        return height;
+    }
+
+    public static int getSceenHeight(Activity activity) {
+        return activity.getWindowManager().getDefaultDisplay().getHeight() + getNavigationBarHeight(activity);
+    }
+
+    /**
+     * 判断手机是否有虚拟按键功能
+     *
+     * @param activity
+     * @return
+     */
+    public static boolean hasNavigationBar(Activity activity) {
+        DisplayMetrics dm = new DisplayMetrics();
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        display.getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+        float density = dm.density;
+
+        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealMetrics(realDisplayMetrics);
+        } else {
+            Class c;
+            try {
+                c = Class.forName("android.view.Display");
+                Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+                method.invoke(display, realDisplayMetrics);
+            } catch (Exception e) {
+                realDisplayMetrics.setToDefaults();
+                e.printStackTrace();
+            }
+        }
+
+        int screenRealHeight = realDisplayMetrics.heightPixels;
+        int screenRealWidth = realDisplayMetrics.widthPixels;
+
+        float diagonalPixels = (float) Math.sqrt(Math.pow(screenWidth, 2) + Math.pow(screenHeight, 2));
+        float screenSize = (diagonalPixels / (160f * density)) * 1f;
+
+        Resources rs = activity.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        boolean hasNavBarFun = false;
+        if (id > 0) {
+            hasNavBarFun = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavBarFun = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavBarFun = true;
+            }
+        } catch (Exception e) {
+            hasNavBarFun = false;
+        }
+        return hasNavBarFun;
+    }
+
+    /**
+     * 检查在有虚拟按键的设备上面，虚拟按键是否展示出来（收起或展示）
+     *
+     * @param windowManager
+     * @return
+     */
+    public static boolean checkDeviceHasNavigationBar(WindowManager windowManager) {
+        DisplayMetrics dm = new DisplayMetrics();
+        Display display = windowManager.getDefaultDisplay();
+        display.getMetrics(dm);
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+
+        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            display.getRealMetrics(realDisplayMetrics);
+        } else {
+            Class c;
+            try {
+                c = Class.forName("android.view.Display");
+                Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+                method.invoke(display, realDisplayMetrics);
+            } catch (Exception e) {
+                realDisplayMetrics.setToDefaults();
+                e.printStackTrace();
+            }
+        }
+        int screenRealHeight = realDisplayMetrics.heightPixels;
+        int screenRealWidth = realDisplayMetrics.widthPixels;
+
+        return (screenRealHeight - screenWidth) > 0;//screenRealHeight上面方法中有计算
     }
 }
